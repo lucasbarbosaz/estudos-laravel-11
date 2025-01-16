85cba7bd6728c0407f50f3c838c7f2801b6d2d1b
Lucas Barbosa <corplaxus@gmail.com>
2025-01-16 14:33:44 -0300

    layouts blade

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>{{$message}}</h1>
+@extends('layouts.app')
+
+
+@section('title', 'Listando usuários')
+@section('content')
+    <h1>{{ $message }}</h1>
 
     @foreach ($users as $user)
-        <div>{{$user->name}} - ({{$user->email}})</div>
+        <div>{{ $user->name }} - ({{ $user->email }})</div>
     @endforeach
-
-</body>
-</html>
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+
+@section('title', 'Mostrar usuário')
+@section('content')
     <h1>Mostrar usuário {{ $user->name }}</h1>
 
     @php
@@ -18,6 +14,4 @@
     @else
         <p>Não sou administrador</p>
     @endif
-
-</body>
-</html>
+@endsection
