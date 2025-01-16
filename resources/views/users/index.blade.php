9ef67ec462ee3d68177765b16ab148607b2c0e02
Lucas Barbosa <corplaxus@gmail.com>
2025-01-16 15:02:44 -0300

    introduction migrates

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
 @section('title', 'Listando usuários')
 @section('content')
     <h1 class="title">{{ $message }}</h1>
-    <img src="{{ Vite::asset('resources/images/carro-dosonho.jpg') }}">
+    <img width="100" src="{{ Vite::asset('resources/images/carro-dosonho.jpg') }}">
     @foreach ($users as $user)
         <div class="user-name">{{ $user->name }} - ({{ $user->email }})</div>
     @endforeach
