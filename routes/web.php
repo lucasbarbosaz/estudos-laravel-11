dbc765f3001f1d7c1fdfc3704f4458528523a689
Lucas Barbosa <corplaxus@gmail.com>
2025-01-16 22:45:57 -0300

    simple elequent create data

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    // $post = new Post();
+    // $post->title = 'Meu primeiro post';
+    // $post->body = 'Texto Texto';
+    // $post->save();
+    $post = Post::create([
+        'title' => 'Meu segundo post',
+        'body' => 'Texto Texto'
+    ]);
+
+    dd($post);
+
     return view('welcome');
 });
 
